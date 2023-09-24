c45f92e61f20479662c9c7407f10f92780ef5047
Matt Holmes <78521446+matthewjholmes@users.noreply.github.com>
2023-09-23 21:59:29 -0700

    Restructure model tests

--- spec/models/astronaut_spec.rb
+++ spec/models/astronaut_spec.rb
@@ -27,23 +27,19 @@ describe Astronaut, type: :model do
   describe 'Instance Methods' do
     before do 
       @neil = Astronaut.create!(name: "Neil Armstrong", age: 37, job: "Commander")
-      @astro2 = Astronaut.create!(name: "Astronaut 2", age: 33, job: "Janitor")
-      @astro3 = Astronaut.create!(name: "Astronaut 3", age: 28, job: "Breath Holder")
 
       @mission_a = Mission.create!(title: "A", time_in_space: 5)
       @mission_b = Mission.create!(title: "B", time_in_space: 10)
       @mission_c = Mission.create!(title: "C", time_in_space: 15)
+    
+      @neil.missions << [@mission_c, @mission_b, @mission_a]
     end
 
     it '#alpha_missions' do
-      @neil.missions << [@mission_c, @mission_b, @mission_a]
-
       expect(@neil.alpha_missions).to eq([@mission_a, @mission_b, @mission_c])
     end
 
-    it '#time_in_space' do
-      @neil.missions << [@mission_c, @mission_b, @mission_a]
-      
+    it '#time_in_space' do      
       expect(@neil.time_in_space).to eq(30)
     end
   end
